711391f00547c953a22a38445457f5152e25b565
Prabhjot Singh <prabhjots2005@gmail.com>
2025-12-31 20:33:45 +0530

    practice

---
+++ full stack web dev/React/280-jsx-code-challenge/src/index.jsx
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>hello</h1>
+    <ul>
+      <li>h</li>
+      <li>a</li>
+      <li>b</li>
+    </ul>
+  </div>
+);
