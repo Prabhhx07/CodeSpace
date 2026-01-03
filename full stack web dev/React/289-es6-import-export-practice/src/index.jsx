2951a51509f2e7f3c9c0ae1a065d7ec30d51f6d6
Prabhjot Singh <prabhjots2005@gmail.com>
2026-01-04 03:26:39 +0530

    import export practice

---
+++ full stack web dev/React/289-es6-import-export-practice/src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { add, multiply, subtract, divide } from "./calculator";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>
+);
