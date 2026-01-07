278b38a03ccc0e23a233a080467a539d6dc1aff9
Prabhjot Singh <prabhjots2005@gmail.com>
2026-01-07 22:09:34 +0530

    .

---
+++ full stack web dev/React/299-mapping-components-practice/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import emojipedia from ".emojipedia.js"
+import Emoji from "./emoji";
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      {emoji.map()}
+
+    </div>
+  );
+}
+
+export default App;
